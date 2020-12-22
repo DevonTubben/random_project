244465350e19fb474b708a2a07b19ee434ab3fd9
Devon Tubben <tubbend1@msu.edu>
2020-12-22 13:56:52 -0500

    added crypto file to bin

---
+++ lib/crypto_cli_project.rb
@@ -0,0 +1,6 @@
+require "crypto_cli_project/version"
+
+module CryptoCliProject
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/crypto_cli_project/version.rb
@@ -0,0 +1,3 @@
+module CryptoCliProject
+  VERSION = "0.1.0"
+end
